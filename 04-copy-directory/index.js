df0f8a5313a8b892befc38673619467089bfb5e2
Anna Perevedentseva <anna.pereved@gmail.com>
2025-01-19 22:28:12 +0100

    feat: implement task 04-copy-directory

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,31 @@
+const { mkdir, readdir, rm, copyFile } = require('fs/promises');
+const { resolve } = require('path');
+const { stdout } = require('process');
+
+const FOLDER_FROM = 'files';
+const FOLDER_TO = 'files-copy';
+
+const copyDirectory = async (folderFrom, folderTo) => {
+  try {
+    await rm(folderTo, { force: true, recursive: true });
+    await mkdir(resolve(folderTo));
+    const files = await readdir(folderFrom, { withFileTypes: true });
+    for (const file of files) {
+      if (file.isFile()) {
+        copyFile(
+          resolve(__dirname, FOLDER_FROM, file.name),
+          resolve(__dirname, FOLDER_TO, file.name),
+        );
+      } else {
+        copyDirectory(
+          resolve(folderFrom, file.name),
+          resolve(folderTo, file.name),
+        );
+      }
+    }
+  } catch (err) {
+    stdout.write(`Error: ${err.message}`);
+  }
+};
+
+copyDirectory(resolve(__dirname, FOLDER_FROM), resolve(__dirname, FOLDER_TO));
